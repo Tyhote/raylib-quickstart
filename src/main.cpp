df0e444f63f8dba8efaaa543d81b55397fe1c0de
Hazel Dow <hazelmadefresh@gmail.com>
2024-10-27 21:52:39 -0500

    Defined the relevant colors and some data structures

--- src/main.cpp
+++ src/main.cpp
@@ -33,15 +33,26 @@ int main ()
 	// Tell the window to use vysnc and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
+	int screen_res [2] = {1280,800};
+
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "Hello Raylib");
+	InitWindow(screen_res[0], screen_res[1], "Snek by Hazel");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
-
-	// Load a texture from the resources directory
-	Texture wabbit = LoadTexture("wabbit_alpha.png");
 	
+	Color ground_color = WHITE; 
+	Color snake_color = GREEN;
+	Color pellet_color = YELLOW;
+	int max_pellets = 2;
+
+	int snake_position [2] = {0,0};
+	int pellets[max_pellets][2] = {};
+	for(int i=0;i<max_pellets; i++){
+		for(int j = 0; j < 2; j++){
+			pellets[i][j] = 0;
+		}
+	}
 	// game loop
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
@@ -49,13 +60,11 @@ int main ()
 		BeginDrawing();
 
 		// Setup the backbuffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
+		ClearBackground(ground_color);
 		// draw some text using the default font
-		DrawText("Hello Raylib", 200,200,20,WHITE);
-
+		DrawRectangle(0,0,20,20,snake_color);
+		DrawRectangle(20,0,20,20,pellet_color);
 		// draw our texture to the screen
-		DrawTexture(wabbit, 400, 200, WHITE);
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -63,7 +72,6 @@ int main ()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(wabbit);
 
 	// destory the window and cleanup the OpenGL context
 	CloseWindow();
