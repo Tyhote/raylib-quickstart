b18dedbacf152df515e88cbcfd2d9a2b8b3a32b3
Hazel Dow <hazelmadefresh@gmail.com>
2024-10-28 01:32:27 -0500

    idk rn im tired

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,27 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-For a C++ project simply rename the file to .cpp and re-run the build script 
-
--- Copyright (c) 2020-2024 Jeffery Myers
---
---This software is provided "as-is", without any express or implied warranty. In no event 
---will the authors be held liable for any damages arising from the use of this software.
-
---Permission is granted to anyone to use this software for any purpose, including commercial 
---applications, and to alter it and redistribute it freely, subject to the following restrictions:
-
---  1. The origin of this software must not be misrepresented; you must not claim that you 
---  wrote the original software. If you use this software in a product, an acknowledgment 
---  in the product documentation would be appreciated but is not required.
---
---  2. Altered source versions must be plainly marked as such, and must not be misrepresented
---  as being the original software.
---
---  3. This notice may not be removed or altered from any source distribution.
-
-*/
-
 #include "raylib.h"
 
-#include "resource_dir.h"	// utility header for SearchAndSetResourceDir
+#include "resource_dir.h"	
+
+class Pellet {
+	public:
+		int GetID();
+		int[] GetLocation();
+		void Move();
+		~Pellet();
+	
+	private:
+		int ID;
+		int location [];
+
+};
+
+int Pellet::GetID(){
+	
+}
+
+int[] Pellet::GetLocation(){
+	return &self.location;
+}
 
 int main ()
 {
@@ -34,26 +29,32 @@ int main ()
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	int screen_res [2] = {1280,800};
-
 	// Create the window and OpenGL context
 	InitWindow(screen_res[0], screen_res[1], "Snek by Hazel");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 	
+	enum Direction { up, down, left, right };
+
+		
 	Color ground_color = WHITE; 
 	Color snake_color = GREEN;
 	Color pellet_color = YELLOW;
 	int max_pellets = 2;
+	int max_segments = 10;
 
-	int snake_position [2] = {0,0};
+	int snake_head_position [2] = {0,0};
+	Direction snake_direction = up;
+
+	// Initializing the pellets
 	int pellets[max_pellets][2] = {};
 	for(int i=0;i<max_pellets; i++){
 		for(int j = 0; j < 2; j++){
 			pellets[i][j] = 0;
 		}
 	}
-	// game loop
+	
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
 		// drawing
@@ -61,7 +62,7 @@ int main ()
 
 		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(ground_color);
-		// draw some text using the default font
+		
 		DrawRectangle(0,0,20,20,snake_color);
 		DrawRectangle(20,0,20,20,pellet_color);
 		// draw our texture to the screen
@@ -69,11 +70,8 @@ int main ()
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
-
-	// cleanup
-	// unload our texture so it can be cleaned up
-
-	// destory the window and cleanup the OpenGL context
+	
+	// Destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
 }
