f4c928571a9a26bf2896bb6a81f27ece531beb01
Hazel Dow <hazelmadefresh@gmail.com>
2024-11-01 02:39:47 -0500

    refactored some stuff for deltav tick logic

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@ struct Pellet{
 	int size;
 };
 
+struct Snake{
+	Vector2 position;
+	int size;
+	float speed;
+};
+
+void UpdateSnake(Snake *snake, Pellet *pellets, int numPellets, float delta);
+
 int main ()
 {
 	// Tell the window to use vysnc and work on high DPI displays
@@ -20,19 +28,16 @@ int main ()
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 	
-	enum Direction { up, down, left, right, none };
-
 		
 	Color ground_color = WHITE; 
 	Color snake_color = GREEN;
 	Color pellet_color = YELLOW;
 	int max_pellets = 2;
 	int max_segments = 10;
-	int snake_size = 20;
-	Direction snake_direction = Direction.left;
+	Snake snake;
 
-	int snake_head_position [2] = {screen_res[0]/2,screen_res[1]/2};
-	
+	snake.position = {screen_res[0]/2,screen_res[1]/2};
+	snake.size = 20;
 	std::unordered_map<int,Pellet> pellets;
 	for(int i=0; i<max_pellets; i++){
 		pellets[i].position = {0,0};
@@ -42,35 +47,12 @@ int main ()
 
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
-		int keypress;
-		Direction new_direction = Direction.none;
-		do {
-			keypress = GetKeyPressed();
-		}
-		// Getting input to move the snake, discards all but the most recent input
-		while(keypress != 0){
-			switch(keypress){
-				case 37:
-					snake_direction = Direction.left;
-					break;
-				case 38:
-					snake_direction = Direction.up;
-					break;
-				case 39:
-					snake_direction = Direction.right;
-					break;
-				case 40:
-					snake_direction = Direction.down;
-					break;
-			}
-		}
-		
 		// drawing
 		BeginDrawing();
 		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(ground_color);
 		// Drawing the snake
-		DrawRectangle(snake_head_position[0],snake_head_position[1],snake_size,snake_size,snake_color);
+		DrawRectangle(snake.position.x,snake.position.y,snake.size,snake.size,snake_color);
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
 	}
@@ -78,4 +60,8 @@ int main ()
 	// Destroy the window and cleanup the OpenGL context
 	CloseWindow();
 	return 0;
+}
+
+void UpdateSnake(Snake *snake, Pellet *pellets, int numPellets, float delta){
+	if(IsKeyDown(KEY_LEFT)) snake->position.x -= snake->speed*delta;
 }
