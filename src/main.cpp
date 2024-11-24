aa0ef4438cdb30a4db60b42d603dda734ba514e9
Hazel Dow <hazelmadefresh@gmail.com>
2024-11-24 02:45:54 -0600

    Started work on giving the snake segments and collision

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "resource_dir.h"
 
 #define MAX_SEGMENTS 10
+#define MAX_TURNS 2000
 
 enum Direction {up, down, left, right, no};
 
@@ -16,14 +17,15 @@ struct Snake{
 	Vector2 position;
 	int size;
 	int segments;
-	int (*turns)[2];
+	int turns;
+	Vector2 tp[MAX_TURNS];
 
 	float speed;
 	Direction direction = no;
 };
 
 
-void UpdateSnake(Snake *snake, int numSegments , Pellet *pellets, int numPellets, float delta);
+void UpdateSnake(Snake *snake, int numSegments , Pellet *pellets, int numPellets);
 Direction GetDirection(Direction currentDirection);
 
 int main ()
@@ -43,12 +45,13 @@ int main ()
 	Color snake_color = GREEN;
 	Color pellet_color = YELLOW;
 	int max_pellets = 2;
-	int max_segments = 10;
+	int max_segments = MAX_SEGMENTS;
 	Snake *snake = new Snake();
 
 	snake->position = {screen_res[0]/2,screen_res[1]/2};
 	snake->size = 20;
 	snake->segments = 0;
+	snake->turns = 0;
 	snake->speed = 80;
 	snake->direction = no;
 
@@ -59,15 +62,12 @@ int main ()
 
 	while (!WindowShouldClose())		// run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
-		snake->direction = GetDirection(snake->direction);
-		float deltaTime = GetFrameTime();
-
 		// drawing
 		BeginDrawing();
 		// Setup the backbuffer for drawing (clear color and depth buffers)
 		ClearBackground(ground_color);
 		// Drawing the snake
-		UpdateSnake(snake,num_segments,pellets,num_pellets,deltaTime);
+		UpdateSnake(snake,num_segments,pellets,num_pellets);
 		DrawRectangle(snake->position.x,snake->position.y,snake->size,snake->size,snake_color);
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
@@ -80,16 +80,37 @@ int main ()
 	delete snake;
 }
 
-void UpdateSnake(Snake *snake, int numSegments, Pellet *pellets, int numPellets, float delta){
+void UpdateSnake(Snake *snake, int numSegments, Pellet *pellets, int numPellets){		
+	// Storing prev direction temporarily to save on computation
+	Direction prev_direction = snake->direction;
+	// Then getting the potentially new direction from user input
+	snake->direction = GetDirection(prev_direction);
+	
+	// Get time passed since last frame
+	float deltaTime = GetFrameTime();
+
+	// If the player has turned the snake, store the point at which the player turned
+	if(prev_direction != snake->direction){
+		snake->tp[snake->turns] = snake->position;
+		snake->turns++;
+	}
+
+	// If the game hasn't started, don't move the snake
 	if(snake->direction == no) return;
+	//	Otherwise move head in respective directions
 	if(snake->direction == left) snake->position.x -= snake->speed*delta;
 	if(snake->direction == right) snake->position.x += snake->speed*delta;
 	if(snake->direction == up) snake->position.y -= snake->speed*delta;
 	if(snake->direction == down) snake->position.y += snake->speed*delta;
-}
 
-void Collision(Snake *snake, Pellet *pellets){
-	
+	Vector2 start = snake->position;
+	// Now we propagate back from the turning points
+	for(int i = 0; i < snake->turns; i++){
+		// Find the distance we need to span for each length of the snake
+		
+		// Within line we place as many segments as possible
+
+	}
 }
 
 Direction GetDirection(Direction currentDirection){
